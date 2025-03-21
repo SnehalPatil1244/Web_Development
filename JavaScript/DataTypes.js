c94b621e23e7f150013e6afaf702f2bccca2f87d
patilsnehal <patilsnehal1469@gmail.com>
2025-03-21 12:46:20 +0530

    Web Development

---
+++ JavaScript/DataTypes.js
@@ -0,0 +1,17 @@
+let num = 10;
+let str = "javaScript";
+let bool = true;
+let obj = {name:"John",age:25};
+let arr = [1,2,3];
+let undef;
+let nul = null;
+let func = function() {};
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof obj);
+console.log(typeof arr);
+console.log(typeof undef);
+console.log(typeof nul);
+console.log(typeof func);
---
+++ JavaScript/If-Else.js
@@ -0,0 +1,12 @@
+let number =10;
+if(number > 0)
+{
+    console.log("The Number Is Positive.");
+}
+else if(number < 0)
+{
+    console.log("The Number Is Negative.");
+}
+else{
+    console.log("The Number Is Zero.");
+}
