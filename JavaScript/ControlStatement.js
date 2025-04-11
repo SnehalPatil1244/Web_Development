659be877b7d7e7fc477dc3120012d561aafc0cfe
patilsnehal <patilsnehal1469@gmail.com>
2025-04-11 21:26:16 +0530

    Web Development

---
+++ JavaScript/ControlStatement.js
@@ -0,0 +1,23 @@
+//Conditional Statement
+/*let age = 25;
+if(age > 18)
+{
+    console.log("Vote");
+}
+if(age < 18)
+{
+    console.log("NOT Vote");
+}*/
+let mode = "light";
+let color;
+if (mode === "Dark")
+{
+    color = "Black";
+}
+else 
+{
+    color = "White";
+}
+console.log(color);
+
+
---
+++ JavaScript/Operators.js
@@ -0,0 +1,55 @@
+let a = 5;
+let b = 5;
+console.log("a = ", a ,"& b = ",b);
+console.log("a  + b  = ", a + b);
+console.log("a - b = ",a - b);
+console.log("a * b = ",a * b);
+console.log("a / b = ",a / b);
+console.log("a % b = ",a % b);
+console.log("a ** b = ", a ** b);
+
+//uniary Operator
+a++;
+console.log("a++ =",a++);
+b--;
+console.log("--b = ",--b);
+
+//Assignment Operators
+
+a += 4;
+console.log("a = ",a);
+b -= 4;
+console.log("b = ",b);
+a *= 2;
+console.log("a =",a);
+b %= 2;
+console.log("a = ",a);
+a **= 2;
+console("a = ",a);
+
+//Comparsion Operators
+
+console.log("a = ", a ,"& b = ",b);
+console.log("a == b =", a == b);
+console.log("a != b = ",a != b);
+console.log("a === b", a === b);
+console.log("a !== b", a !== b);
+
+
+console.log("a = ", a ,"& b = ",b);
+console.log("a < b = ", a < b);
+console.log("a > b = ",a > b);
+console.log("a >= b =", a >= b);
+console.log("a <= b  =",a <= b);
+
+//Logical Operators
+
+
+
+let cond1 = a  > b;
+let cond2 = a === b;
+
+console.log("cond1 && cond2 = ", a > b && a === b);
+console.log("cond1 || cond2 = ", a > b || a === b);
+console.log("!(a > b) =", !(a > b));
+
